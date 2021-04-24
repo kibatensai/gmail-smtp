b2b0bfd2ab9de56e04fc4de126ca59ff0650384d
Kibatensai <lenutd@yandex.ru>
2021-04-25 01:24:20 +0300

    gmail-smtp

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const nodemailer = require('nodemailer')
+const cors = require('cors')
+const app = express()
+const port = 3010
+
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// SMTP Transporter
+let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "dave.p.nodejs@gmail.com",
+        pass: "nodejslddp5t4v"
+    },
+    tls: {
+        rejectUnauthorized: false
+      }
+})
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.post('/sendMessage', async (req, res) => {
+    let {name, email, message} = req.body
+
+    try {
+    let info = await transporter.sendMail({
+        from: "Guest of CV Website", // who sends
+        to: "dave.p.business@gmail.com", //who receives
+        subject: "EMAIL FROM CV WEBSITE", // subject line
+        html: `
+            <b>Письмо от интересанта с моего сайта CV </b>
+            <br />
+            <div>
+                <b>Email: </b> ${email}
+            </div>
+            <br/>
+            <div>
+                <b>Name: </b> ${name}
+            </div>
+            <br/>
+            <div>
+                <b>Message: </b>${message}
+            </div>`
+
+    })
+    console.log('finished')
+
+    } catch (error) {
+        console.log('error : ' + error)
+    }
+    res.send('ok')
+
+
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
